609acf492ab11048f4daf9517ca6c9b3d976e2a6
PaoloSalvatico <paolosalvatico@icloud.com>
2021-11-19 11:13:44 +0100

    add bullet item

---
+++ Assets/Script/Inventory/BulletItem.cs
@@ -0,0 +1,15 @@
+
+namespace TheFirstGame.InventorySystem
+{
+    /// <summary>
+    ///  Permete di gestire i proiettili e il loro numero all'interno dell'inventario
+    /// </summary>
+    [System.Serializable]
+    public struct BulletItem
+    {
+        public string name;
+        public string weaponName;
+        public int count;
+    }
+}
+
